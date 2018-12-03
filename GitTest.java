77ac8cd629afabd38daad64198f1b310d9dc7237
nikethLakshmanan <45395176+nikethLakshmanan@users.noreply.github.com>
2018-12-03 10:20:33 -0500

    add

--- GitTest.java
+++ GitTest.java
@@ -1,5 +1,5 @@
-//Niketh Lakshmanan 
-public class GitTest{ 
+//Lakshmanan
+public class GitTest{
 	private String sentence;
 	public GitTest(String sentence){
 		sentence = "this is my GitHub test";
